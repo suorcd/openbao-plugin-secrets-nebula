54b1bcb1575b38597912262b9de422cf537e510c
suorcd <github@bobot.lol>
2026-06-29 15:29:05 -0700

    init flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "OpenBao Secrets Engine for Nebula";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          default = openbao-plugin-secrets-nebula;
+
+          openbao-plugin-secrets-nebula = pkgs.buildGoModule {
+            pname = "openbao-plugin-secrets-nebula";
+            version = "2.0.0"; # Bumping to v2 for Nebula certs
+
+            src = ./.;
+
+            # We target the main command package specifically
+            subPackages = [ "cmd/openbao-plugin-secrets-nebula" ];
+
+            # Nix requires the vendor hash to guarantee reproducibility.
+            # Leave this as fakeHash for the first build. It will fail and
+            # give you the real hash, which you will paste here.
+            vendorHash = pkgs.lib.fakeHash;
+
+            meta = with pkgs.lib; {
+              description = "OpenBao Secrets Engine for Nebula PKI";
+              homepage = "https://github.com/suorcd/openbao-plugin-secrets-nebula";
+              license = licenses.mpl20;
+              maintainers = [ ];
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          # This gives you a perfect local dev environment when you run `nix develop`
+          buildInputs = with pkgs; [
+            go # Latest stable Go from nixpkgs
+            goreleaser # For building your Github releases
+            gnumake
+            gotools
+            golangci-lint
+          ];
+
+          shellHook = ''
+            echo "OpenBao Nebula Plugin Dev Environment loaded."
+            echo "Go version: $(go version)"
+          '';
+        };
+      }
+    );
+}
